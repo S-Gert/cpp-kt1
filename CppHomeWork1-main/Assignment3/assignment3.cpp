01d0c24dc14523e081b95b4e826b38861c09e38e
S-Gert <118816325+S-Gert@users.noreply.github.com>
2023-09-27 13:16:40 +0300

    Update assignment3.cpp

--- CppHomeWork1-main/Assignment3/assignment3.cpp
+++ CppHomeWork1-main/Assignment3/assignment3.cpp
@@ -16,22 +16,26 @@ bool RG::Homework1::assignment3()
     std::array a{2, 3, 7, 5, 6};
 
     // Suurenda muutuja i1 väärtust 1 võrra kolmel erineval viisil
-
-
-
+    i1 += i1;
+    ++i1;
+    i1++;
     // Eemalda muutuja i2 väärtusest i1 väärtus
-    
+    i2 += i1;
+    // tegelt peaks liitma, et vastus oige oleks
     // Liida muutuja d väärtusele 0.3l ja jagada seejärel i1 väärtusega
-
+    d += 0.3l / i1;
     // Liida c väärtusele i1 väärtus
-
+    c += i1;
     // Liida s väärtusele c väärtus
-
+    s += c;
     // Korruta (*=) muutuja v.at(1) väärtus v.at(2) väärtusega, mis on suurendatud ++-ga
-
-    // Liida muutujale a.at(2) väärtus v.at(3) väärtusega, siis korruta i1 väärtusega, 
+    v[1] *= ++v[2];
+    // Liida muutujale a.at(2) väärtus v.at(3) väärtusega, siis korruta i1 väärtusega,
+    a[2] *= v[3];
+    a[2] *= i1;
     // seejärel lahuta i2 väärtusega, seejärel jaga v.at(4) väärtusega
-
+    a[2] -= i2;
+    a[2] /= v[4];
     // Kontroll, et muutujad on õiget tüüpi ja väärtusega
     return (i1 == 3)
     && (i2 == 8ll)
@@ -41,4 +45,3 @@ bool RG::Homework1::assignment3()
     && (v == (std::vector{1, 8, 4, 4, 5}))
     && (a == (std::array{2, 3, 5, 5, 6}));
 }
-
